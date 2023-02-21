a6b87f4c215be2c3c7e1a1b56c660d708f673fcd
motero2k <manolo.ob@hotmail.com>
2023-02-21 17:19:52 +0100

    Implement Lecturer role

---
+++ src/main/java/acme/roles/Lecturer.java
@@ -0,0 +1,43 @@
+
+package acme.roles;
+
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.URL;
+
+import acme.framework.data.AbstractRole;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Lecturer extends AbstractRole {
+
+	// Serialisation identifier -----------------------------------------------
+
+	protected static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+
+	@NotBlank
+	@Size(max = 76, message = "Must be shorter than 76 characters")
+	protected String			almaMater;
+
+	@NotBlank
+	@Size(max = 101, message = "Must be shorter than 101 characters")
+	protected String			resume;
+
+	@NotBlank
+	@Size(max = 76, message = "Must be shorter than 101 characters")
+	protected String			qualifications;
+	@URL(message = "Must be a valid URL")
+	protected String			link;
+
+	// Derived attributes -----------------------------------------------------
+
+	// Relationships ----------------------------------------------------------
+	//TODO
+}
