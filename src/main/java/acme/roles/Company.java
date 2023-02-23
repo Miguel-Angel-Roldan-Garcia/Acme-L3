b00d06b8bb3ab6ec2028395ec964f839faf76256
JaviFdez7 <javidelatorre400@gmail.com>
2023-02-23 18:15:11 +0100

    fix attribute name and restrictions labels

--- src/main/java/acme/roles/Company.java
+++ src/main/java/acme/roles/Company.java
@@ -4,7 +4,9 @@ package acme.roles;
 import javax.persistence.Entity;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
+import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.URL;
 
 import acme.framework.data.AbstractRole;
@@ -18,24 +20,27 @@ public class Company extends AbstractRole {
 
 	// Serialisation identifier -----------------------------------------------
 
-	protected static final long	serialVersionUID	= 1L;
+	protected static final long serialVersionUID = 1L;
 
 	// Attributes -------------------------------------------------------------
 
+	@NotNull
 	@NotBlank
-	@Max(value = 76, message = "Must be shorter than 76 characters")
-	protected String			name;
+	@Length(max = 76)
+	protected String name;
 
+	@NotNull
 	@NotBlank
-	@Max(value = 26, message = "Must be shorter than 26 characters")
-	protected String			ivaNum;
+	@Length(max = 26)
+	protected String vatNumber;
 
+	@NotNull
 	@NotBlank
-	@Max(value = 101, message = "Must be shorter than 101 characters")
-	protected String			resume;
+	@Max(value = 101)
+	protected String summary;
 
-	@URL(message = "Must be a valid URL")
-	protected String			link;
+	@URL
+	protected String link;
 
 	// Derived attributes -----------------------------------------------------
 
