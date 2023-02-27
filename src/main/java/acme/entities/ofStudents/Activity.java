14e02ed9b4a078324671678708c7c2a808aa06d2
Andrés Domínguez Ruiz <andresdominguezruiz.ad@gmail.com>
2023-02-27 18:41:39 +0100

    Activity preparado, y posible error de analisis encontrado

--- src/main/java/acme/entities/ofStudents/Activity.java
+++ src/main/java/acme/entities/ofStudents/Activity.java
@@ -1,6 +1,39 @@
+
 package acme.entities.ofStudents;
 
+import java.util.Date;
 
-public class Activity {
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotBlank;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.URL;
+
+import acme.framework.data.AbstractEntity;
+
+public class Activity extends AbstractEntity {
+
+	protected static final long	serialVersionUID	= 1L;
+
+	@NotBlank
+	@Length(max = 76)
+	protected String			title;
+
+	/*
+	 * @NotBlank
+	 * 
+	 * @Length(max=101)
+	 * protected String abstract;
+	 */ //posible incorrección, ya que abstract es una palabra clave de java
+
+	@Temporal(TemporalType.TIMESTAMP)
+	protected Date				initialDate;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	protected Date				finishDate;
+
+	@URL
+	protected String			moreInfo;
 
 }
