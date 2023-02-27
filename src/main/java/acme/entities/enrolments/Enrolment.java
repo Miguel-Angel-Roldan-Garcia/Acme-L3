870358de4f3f7591b7d55b74d93e9bf147565a0f
Andrés Domínguez Ruiz <andresdominguezruiz.ad@gmail.com>
2023-02-27 17:35:19 +0100

    Solucionar rol Student y Enrolment creado

--- src/main/java/acme/entities/enrolments/Enrolment.java
+++ src/main/java/acme/entities/enrolments/Enrolment.java
@@ -1,7 +1,10 @@
 
 package acme.entities.enrolments;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 
 import acme.framework.data.AbstractEntity;
 import lombok.Getter;
@@ -12,6 +15,11 @@ import lombok.Setter;
 @Entity
 public class Enrolment extends AbstractEntity {
 
-	protected static final long serialVersionUID = 1L;
+	protected static final long	serialVersionUID	= 1L;
+
+	@Column(unique = true)
+	@NotBlank
+	@Pattern(regexp = "[A-Z]{1,3}[0-9][0-9]{3}")
+	protected String			code;
 
 }
--- src/main/java/acme/roles/Student.java
+++ src/main/java/acme/roles/Student.java
@@ -3,9 +3,9 @@ package acme.roles;
 
 import javax.persistence.Entity;
 import javax.persistence.Table;
-import javax.validation.constraints.Max;
 import javax.validation.constraints.NotBlank;
 
+import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.URL;
 
 import acme.framework.data.AbstractRole;
@@ -21,15 +21,15 @@ public class Student extends AbstractRole {
 	protected static final long	serialVersionUID	= 1L;
 
 	@NotBlank
-	@Max(value = 75, message = "Must be shorter than 76 characters") // he puesto 75 porque Max salo aceptara los valores que son mayores o iguales
+	@Length(max = 76)
 	protected String			statement;
 
 	@NotBlank
-	@Max(value = 100, message = "Must be shorter than 101 characters")
+	@Length(max = 101)
 	protected String			strongFeatures;
 
 	@NotBlank
-	@Max(value = 100, message = "Must be shorter than 101 characters")
+	@Length(max = 101)
 	protected String			weakFeatures;
 
 	@URL
