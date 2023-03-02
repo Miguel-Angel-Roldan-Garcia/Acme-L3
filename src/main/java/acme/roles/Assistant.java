6bb0c7fd8ba52ba8b613e93496e07b6da706ab44
Miguel-Angel-Rolgan-Garcia <migrolgar2@alum.us.es>
2023-03-02 16:44:33 +0100

    Fix on assistant constraints

--- src/main/java/acme/roles/Assistant.java
+++ src/main/java/acme/roles/Assistant.java
@@ -3,7 +3,6 @@ package acme.roles;
 
 import javax.persistence.Entity;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 
 import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.URL;
@@ -15,22 +14,18 @@ public class Assistant extends AbstractRole {
 
 	private static final long	serialVersionUID	= 1L;
 
-	@NotNull
 	@NotBlank
 	@Length(min = 1, max = 101)
 	protected String			supervisor;
 
-	@NotNull
 	@NotBlank
 	@Length(min = 1, max = 101)
 	protected String			expertiseFields;
 
-	@NotNull
 	@NotBlank
 	@Length(min = 1, max = 101)
 	protected String			resume;
 
-	@NotBlank
 	@URL
 	protected String			link;
 
