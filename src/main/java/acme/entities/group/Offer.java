75b5de96d1e74aeeea2277a309713abf2e6919eb
JaviFdez7 <javidelatorre400@gmail.com>
2023-03-14 16:55:15 +0100

    fix de @PastOrPresent y Money price

--- src/main/java/acme/entities/group/Offer.java
+++ src/main/java/acme/entities/group/Offer.java
@@ -8,11 +8,12 @@ import javax.persistence.TemporalType;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Past;
+import javax.validation.constraints.PastOrPresent;
 
 import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.URL;
 
+import acme.framework.components.datatypes.Money;
 import acme.framework.data.AbstractEntity;
 
 public class Offer extends AbstractEntity {
@@ -25,12 +26,12 @@ public class Offer extends AbstractEntity {
 
 	@NotNull
 	@Temporal(TemporalType.TIMESTAMP)
-	@Past
+	@PastOrPresent
 	protected Date instantiationMoment;
 
 	@NotBlank
 	@Length(max = 75)
-	protected String head;
+	protected String heading;
 
 	@NotBlank
 	@Length(max = 100)
@@ -47,7 +48,7 @@ public class Offer extends AbstractEntity {
 	protected Date availabilityPeriodEndDate;
 
 	@Min(value = 0)
-	protected Integer price;
+	protected Money price;
 
 	@URL
 	protected String link;
