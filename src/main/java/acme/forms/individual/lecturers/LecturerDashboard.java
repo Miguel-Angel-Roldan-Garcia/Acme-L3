bd5089658bf0773ba695a8883058c5f2564b585c
motero2k <manolo.ob@hotmail.com>
2023-03-16 14:06:33 +0100

    Lecturer dashboard draft

---
+++ src/main/java/acme/forms/individual/lecturers/LecturerDashboard.java
@@ -0,0 +1,25 @@
+
+package acme.forms.individual.students;
+
+import java.util.Map;
+
+import acme.datatypes.Nature;
+import acme.framework.data.AbstractForm;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class LecturerDashboard extends AbstractForm {
+
+	protected static final long serialVersionUID = 1L;
+
+	//total number of theory lectures
+	//total number of hands-on lectures;
+	Map<Nature,Integer> lecturesCount;
+
+ 	//average, deviation, minimum, and maximum learning time of the lectures;
+	Statistic lectures;
+ 	//average, deviation, minimum, and maximum learning time of the courses.
+	Statistic courses;
+}
