1fd5d93bde34c83a73766907efb9674610904561
JaviFdez7 <javidelatorre400@gmail.com>
2023-03-30 13:36:08 +0200

    Create or update Company

---
+++ src/main/java/acme/features/authenticated/company/AuthenticatedCompanyUpdateService.java
@@ -0,0 +1,79 @@
+
+package acme.features.authenticated.company;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import acme.framework.components.accounts.Authenticated;
+import acme.framework.components.accounts.Principal;
+import acme.framework.components.models.Tuple;
+import acme.framework.controllers.HttpMethod;
+import acme.framework.helpers.PrincipalHelper;
+import acme.framework.services.AbstractService;
+import acme.roles.Company;
+
+@Service
+public class AuthenticatedCompanyUpdateService extends AbstractService<Authenticated, Company> {
+
+	@Autowired
+	protected AuthenticatedCompanyRepository repository;
+
+	@Override
+	public void authorise() {
+		super.getResponse().setAuthorised(true);
+	}
+
+	@Override
+	public void check() {
+		super.getResponse().setChecked(true);
+	}
+
+	@Override
+	public void load() {
+		Company object;
+		Principal principal;
+		int userAccountId;
+
+		principal = super.getRequest().getPrincipal();
+		userAccountId = principal.getAccountId();
+		object = this.repository.findOneCompanyByUserAccountId(userAccountId);
+
+		super.getBuffer().setData(object);
+	}
+
+	@Override
+	public void bind(final Company object) {
+		assert object != null;
+
+		super.bind(object, "name", "vatNumber", "summary", "link");
+	}
+
+	@Override
+	public void validate(final Company object) {
+		assert object != null;
+	}
+
+	@Override
+	public void perform(final Company object) {
+		assert object != null;
+
+		this.repository.save(object);
+	}
+
+	@Override
+	public void unbind(final Company object) {
+		assert object != null;
+
+		Tuple tuple;
+
+		tuple = super.unbind(object, "name", "vatNumber", "summary", "link");
+		super.getResponse().setData(tuple);
+	}
+
+	@Override
+	public void onSuccess() {
+		if (super.getRequest().getMethod().equals(HttpMethod.POST))
+			PrincipalHelper.handleUpdate();
+	}
+
+}
---
+++ src/main/webapp/WEB-INF/views/authenticated/company/form.jsp
@@ -0,0 +1,26 @@
+<%--
+- form.jsp
+-
+- Copyright (C) 2012-2023 Rafael Corchuelo.
+-
+- In keeping with the traditional purpose of furthering education and research, it is
+- the policy of the copyright owner to permit non-commercial use and redistribution of
+- this software. It has been tested carefully, but it is not guaranteed for any particular
+- purposes.  The copyright owner does not offer any warranties or representations, nor do
+- they accept any liabilities with respect to them.
+--%>
+
+<%@page language="java"%>
+
+<%@taglib prefix="jstl" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@taglib prefix="acme" uri="http://www.the-acme-framework.org/"%>
+
+<acme:form>
+	<acme:input-textarea code="authenticated.company.form.label.name" path="name"/>
+	<acme:input-textarea code="authenticated.company.form.label.vat-number" path="vatNumber"/>
+	<acme:input-textarea code="authenticated.company.form.label.summary" path="summary"/>
+	<acme:input-url code="authenticated.company.form.label.link" path="link"/>
+	
+	<acme:submit test="${_command == 'create'}" code="authenticated.company.form.button.create" action="/authenticated/company/create"/>
+	<acme:submit test="${_command == 'update'}" code="authenticated.company.form.button.update" action="/authenticated/company/update"/>
+</acme:form>
