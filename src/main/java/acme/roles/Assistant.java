febc5c8b3a3e3b1bc4cf55651ce47fdbb8e8b789
Miguel-Angel-Rolgan-Garcia <migrolgar2@alum.us.es>
2023-03-30 16:37:25 +0200

    Assistant fix

--- src/main/java/acme/roles/Assistant.java
+++ src/main/java/acme/roles/Assistant.java
@@ -8,23 +8,37 @@ import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.URL;
 
 import acme.framework.data.AbstractRole;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
 public class Assistant extends AbstractRole {
 
+	// Serialisation identifier -----------------------------------------------
+
 	private static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
 
 	@NotBlank
 	@Length(max = 75)
 	protected String			supervisor;
 
 	@Length(max = 100)
+	@NotBlank
 	protected String			expertiseFields;
 
 	@Length(max = 100)
+	@NotBlank
 	protected String			resume;
 
 	@URL
 	protected String			link;
+
+	// Derived attributes -----------------------------------------------------
+
+	// Relationships ----------------------------------------------------------
 
 }
