007da6031022bf4a68aa0a9bd7f49c94b23dd517
JaviFdez7 <javidelatorre400@gmail.com>
2023-04-10 18:29:18 +0200

    update

--- src/main/java/acme/roles/Assistant.java
+++ src/main/java/acme/roles/Assistant.java
@@ -8,25 +8,37 @@ import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.URL;
 
 import acme.framework.data.AbstractRole;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
 public class Assistant extends AbstractRole {
 
+	// Serialisation identifier -----------------------------------------------
+
 	private static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
 
 	@NotBlank
 	@Length(max = 75)
 	protected String			supervisor;
 
-	@NotBlank
 	@Length(max = 100)
+	@NotBlank
 	protected String			expertiseFields;
 
-	@NotBlank
 	@Length(max = 100)
+	@NotBlank
 	protected String			resume;
 
 	@URL
 	protected String			link;
+
+	// Derived attributes -----------------------------------------------------
+
+	// Relationships ----------------------------------------------------------
 
 }
