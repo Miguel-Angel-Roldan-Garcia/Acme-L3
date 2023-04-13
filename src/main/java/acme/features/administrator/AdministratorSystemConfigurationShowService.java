7923f984c3c25b3247f55522ec2150eff30487ef
Miguel-Angel-Rolgan-Garcia <migrolgar2@alum.us.es>
2023-04-13 20:53:32 +0200

    System configuration operations implemented

---
+++ src/main/java/acme/features/administrator/AdministratorSystemConfigurationShowService.java
@@ -0,0 +1,70 @@
+/*
+ * WorkerApplicationShowService.java
+ *
+ * Copyright (C) 2012-2023 Rafael Corchuelo.
+ *
+ * In keeping with the traditional purpose of furthering education and research, it is
+ * the policy of the copyright owner to permit non-commercial use and redistribution of
+ * this software. It has been tested carefully, but it is not guaranteed for any particular
+ * purposes. The copyright owner does not offer any warranties or representations, nor do
+ * they accept any liabilities with respect to them.
+ */
+
+package acme.features.administrator;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import acme.entities.group.SystemConfiguration;
+import acme.framework.components.accounts.Administrator;
+import acme.framework.components.models.Tuple;
+import acme.framework.services.AbstractService;
+
+@Service
+public class AdministratorSystemConfigurationShowService extends AbstractService<Administrator, SystemConfiguration> {
+
+	// Internal state ---------------------------------------------------------
+
+	@Autowired
+	protected AdministratorSystemConfigurationRepository repository;
+
+	// AbstractService interface ----------------------------------------------
+
+
+	@Override
+	public void check() {
+		super.getResponse().setChecked(true);
+	}
+
+	@Override
+	public void authorise() {
+		boolean status;
+		SystemConfiguration systemConfiguration;
+
+		systemConfiguration = this.repository.findSystemConfiguration();
+		status = systemConfiguration != null && super.getRequest().getPrincipal().hasRole(Administrator.class);
+
+		super.getResponse().setAuthorised(status);
+	}
+
+	@Override
+	public void load() {
+		SystemConfiguration object;
+
+		object = this.repository.findSystemConfiguration();
+
+		super.getBuffer().setData(object);
+	}
+
+	@Override
+	public void unbind(final SystemConfiguration object) {
+		assert object != null;
+
+		Tuple tuple;
+
+		tuple = super.unbind(object, "currency", "acceptedCurrencies");
+
+		super.getResponse().setData(tuple);
+	}
+
+}
