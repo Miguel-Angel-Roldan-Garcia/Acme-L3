617685f4bdcdfc62593ac3eebdb975a78a08d162
Miguel-Angel-Rolgan-Garcia <migrolgar2@alum.us.es>
2023-05-21 15:05:15 +0200

    Peep tests

---
+++ src/test/java/acme/testing/any/peep/AnyCreateTest.java
@@ -0,0 +1,87 @@
+/*
+ * AssistantTutorialListMineTest.java
+ *
+ * Copyright (C) 2022-2023 Miguel Ángel Roldán.
+ *
+ * In keeping with the traditional purpose of furthering education and research, it is
+ * the policy of the copyright owner to permit non-commercial use and redistribution of
+ * this software. It has been tested carefully, but it is not guaranteed for any particular
+ * purposes. The copyright owner does not offer any warranties or representations, nor do
+ * they accept any liabilities with respect to them.
+ */
+
+package acme.testing.any.peep;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+import acme.testing.TestHarness;
+
+public class AnyCreateTest extends TestHarness {
+
+	@ParameterizedTest
+	@CsvFileSource(resources = "/any/peep/create-positive.csv", encoding = "utf-8", numLinesToSkip = 1)
+	public void test100Positive(final int recordIndex, final String title, final String nick, final String message, final String email, final String link) {
+		// HINT: this test lists all peeps, creates a new one 
+		// HINT: and checks that it's been created properly.
+
+		super.clickOnMenu("Any", "List published peeps");
+		super.checkListingExists();
+		super.sortListing(2, "asc");
+
+		if (recordIndex >= 27)
+			System.out.println("");
+
+		super.clickOnButton("Create");
+		super.checkFormExists();
+		super.fillInputBoxIn("title", title);
+		super.fillInputBoxIn("nick", nick);
+		super.fillInputBoxIn("message", message);
+		super.fillInputBoxIn("email", email);
+		super.fillInputBoxIn("link", link);
+		super.clickOnSubmit("Create");
+
+		super.clickOnMenu("Any", "List published peeps");
+		super.checkListingExists();
+		super.sortListing(2, "asc");
+
+		super.checkColumnHasValue(recordIndex, 1, nick);
+		super.checkColumnHasValue(recordIndex, 2, title);
+
+		super.clickOnListingRecord(recordIndex);
+		super.checkFormExists();
+		super.checkInputBoxHasValue("title", title);
+		super.checkInputBoxHasValue("nick", nick);
+		super.checkInputBoxHasValue("message", message);
+		super.checkInputBoxHasValue("email", email);
+		super.checkInputBoxHasValue("link", link);
+	}
+
+	@ParameterizedTest
+	@CsvFileSource(resources = "/any/peep/create-negative.csv", encoding = "utf-8", numLinesToSkip = 1)
+	public void test200Negative(final int recordIndex, final String title, final String nick, final String message, final String email, final String link) {
+		// HINT: this test attempts to create peeps with incorrect data.
+
+		super.clickOnMenu("Any", "List published peeps");
+		super.checkListingExists();
+		super.sortListing(2, "asc");
+
+		super.clickOnButton("Create");
+		super.checkFormExists();
+		super.fillInputBoxIn("title", title);
+		super.fillInputBoxIn("nick", nick);
+		super.fillInputBoxIn("message", message);
+		super.fillInputBoxIn("email", email);
+		super.fillInputBoxIn("link", link);
+		super.clickOnSubmit("Create");
+		super.checkErrorsExist();
+	}
+
+	@Test
+	public void test300Hacking() {
+		// HINT: there aren't any hacking tests for this feature since it's one that
+		// HINT+ everyone can access, that is, any principals.
+	}
+
+}
